e8d727e6cb15e86a3a7cbe5493ed25a81c722fd5
Mohammed Ayman faroouqi <aimanfarooqui10@gmail.com>
2025-11-07 22:55:15 +0530

    Done with statefile storage in s3

---
+++ Day-07-Testing/main.tf
@@ -0,0 +1,13 @@
+module "name" {
+
+    source = "../Day-07-Modules"
+    ami_id = "ami-0bdd88bd06d16ba03"
+    type = "t3.micro"
+    
+
+
+}
+
+
+
+
---
+++ Day-07-Values/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+
+    backend "s3" {
+
+        bucket = "statefilebackendstorage100"
+        key = "Day-07_2/terraform.tfstate"
+        region = "us-east-1"
+      
+    }
+
+}
