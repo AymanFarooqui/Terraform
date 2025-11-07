7035c7c790dccfbf6b7f98014d32d78274400b3d
Mohammed Ayman faroouqi <aimanfarooqui10@gmail.com>
2025-11-07 23:29:34 +0530

    Done with Day-07

---
+++ Day-07-Module-Source-Terraform/main.tf
@@ -0,0 +1,15 @@
+module "s3_bucket" {
+
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "s3bucket1030devopsmulticloud"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+
+}
